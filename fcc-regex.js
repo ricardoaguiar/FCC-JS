6ada71643e3e66b4755734a8dab55f2f37e9267a
ricardo aguiar <2197501+ricardoaguiar@users.noreply.github.com>
2019-05-24 18:38:40 +0200

    Update fcc-regex.js

--- fcc-regex.js
+++ fcc-regex.js
@@ -7,3 +7,13 @@ let result = myRegex; // Change this line
 //solution
 let result = myRegex.test(myString);
 console.log(result); //true
+
+
+//Regular Expressions: Match Literal Strings
+
+let waldoIsHiding = "Somewhere Waldo is hiding in this text.";
+let waldoRegex = /search/; // Change this line
+let result = waldoRegex.test(waldoIsHiding);
+
+//solution
+let waldoRegex = /Waldo/; // Change this line
