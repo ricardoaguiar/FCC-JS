bb5d82874416c5c1448a5a9c8c440dcb5e3ee707
ricardo aguiar <2197501+ricardoaguiar@users.noreply.github.com>
2019-05-24 18:57:46 +0200

    Update fcc-regex.js

--- fcc-regex.js
+++ fcc-regex.js
@@ -1,19 +1,34 @@
 //using the .test() method
-
 let myString = "Hello, World!";
 let myRegex = /Hello/;
 let result = myRegex; // Change this line
-
 //solution
 let result = myRegex.test(myString);
 console.log(result); //true
 
 
 //Regular Expressions: Match Literal Strings
-
 let waldoIsHiding = "Somewhere Waldo is hiding in this text.";
 let waldoRegex = /search/; // Change this line
 let result = waldoRegex.test(waldoIsHiding);
-
 //solution
 let waldoRegex = /Waldo/; // Change this line
+
+
+//Regular Expressions: Match a Literal String with Different Possibilities
+let petString = "James has a pet cat.";
+let petRegex = /change/; // Change this line
+let result = petRegex.test(petString);
+//solution
+let petString = "James has a pet cat.";
+let petRegex = /dog|cat|bird|fish/; // Change this line
+let result = petRegex.test(petString);
+
+//Regular Expressions: Ignore Case While Matching
+let myString = "freeCodeCamp";
+let fccRegex = /change/; // Change this line
+let result = fccRegex.test(myString);
+//solution
+let myString = "freeCodeCamp";
+let fccRegex = /freeCodeCamp/i; // using the ignore flag /i
+let result = fccRegex.test(myString);
