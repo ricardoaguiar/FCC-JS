027825d7700a312b593f821568ea87cc5217d06c
ricardo aguiar <2197501+ricardoaguiar@users.noreply.github.com>
2019-05-24 19:59:58 +0200

    Update fcc-regex.js

--- fcc-regex.js
+++ fcc-regex.js
@@ -2,53 +2,61 @@
 let myString = "Hello, World!";
 let myRegex = /Hello/;
 let result = myRegex; // Change this line
-//solution
-let result = myRegex.test(myString);
-console.log(result); //true
+  //solution
+  let result = myRegex.test(myString);
+  console.log(result); //true
 
 
 //Regular Expressions: Match Literal Strings
 let waldoIsHiding = "Somewhere Waldo is hiding in this text.";
 let waldoRegex = /search/; // Change this line
 let result = waldoRegex.test(waldoIsHiding);
-//solution
-let waldoRegex = /Waldo/; // Change this line
+  //solution
+  let waldoRegex = /Waldo/; // Change this line
 
 
 //Regular Expressions: Match a Literal String with Different Possibilities
 let petString = "James has a pet cat.";
 let petRegex = /change/; // Change this line
 let result = petRegex.test(petString);
-//solution
-let petString = "James has a pet cat.";
-let petRegex = /dog|cat|bird|fish/; // Change this line
-let result = petRegex.test(petString);
+  //solution
+  let petString = "James has a pet cat.";
+  let petRegex = /dog|cat|bird|fish/; // Change this line
+  let result = petRegex.test(petString);
 
 //Regular Expressions: Ignore Case While Matching
 let myString = "freeCodeCamp";
 let fccRegex = /change/; // Change this line
 let result = fccRegex.test(myString);
-//solution
-let myString = "freeCodeCamp";
-let fccRegex = /freeCodeCamp/i; // using the ignore flag /i
-let result = fccRegex.test(myString);
+  //solution
+  let myString = "freeCodeCamp";
+  let fccRegex = /freeCodeCamp/i; // using the ignore flag /i
+  let result = fccRegex.test(myString);
 
 //Regular Expressions: Extract Matches
 let extractStr = "Extract the word 'coding' from this string.";
 let codingRegex = /change/; // Change this line
 let result = extractStr; // Change this line
-//example
-"Hello, World!".match(/Hello/);
-// Returns ["Hello"]
-let ourStr = "Regular expressions";
-let ourRegex = /expressions/;
-ourStr.match(ourRegex);
-// Returns ["expressions"]
-//solution
-let extractStr = "Extract the word 'coding' from this string.";
-let codingRegex = /coding/; // Change this line
-let result = extractStr.match(codingRegex); // Change this line
-console.log(result); //coding
+    //example
+    "Hello, World!".match(/Hello/);
+    // Returns ["Hello"]
+    let ourStr = "Regular expressions";
+    let ourRegex = /expressions/;
+    ourStr.match(ourRegex);
+    // Returns ["expressions"]
+      //solution
+      let extractStr = "Extract the word 'coding' from this string.";
+      let codingRegex = /coding/; // Change this line
+      let result = extractStr.match(codingRegex); // Change this line
+      console.log(result); //coding
 
-
+//Regular Expressions: Find More Than the First Match
+let twinkleStar = "Twinkle, twinkle, little star";
+let starRegex = /change/; // Change this line
+let result = twinkleStar; // Change this line
+  //solution
+  let twinkleStar = "Twinkle, twinkle, little star";
+  let starRegex = /Twinkle/gi; // Change this line // using the global (g) and ignore (i) flags
+  let result = twinkleStar.match(starRegex); // Change this line
+  console.log(result); // return Twinkle, twinkle
 
