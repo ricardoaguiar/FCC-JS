04ff74c15eff38fdec4f383002d2f8315561dc76
ricardo aguiar <2197501+ricardoaguiar@users.noreply.github.com>
2019-05-24 19:04:40 +0200

    Update fcc-regex.js

--- fcc-regex.js
+++ fcc-regex.js
@@ -37,3 +37,15 @@ let result = fccRegex.test(myString);
 let extractStr = "Extract the word 'coding' from this string.";
 let codingRegex = /change/; // Change this line
 let result = extractStr; // Change this line
+//example
+"Hello, World!".match(/Hello/);
+// Returns ["Hello"]
+let ourStr = "Regular expressions";
+let ourRegex = /expressions/;
+ourStr.match(ourRegex);
+// Returns ["expressions"]
+//solution
+
+
+
+
