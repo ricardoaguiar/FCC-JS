76abca319952a3c12c3797b5917574a5dcbc6272
ricardo aguiar <2197501+ricardoaguiar@users.noreply.github.com>
2019-05-26 17:24:31 +0200

    Update data-structure.js

--- data/data-structure.js
+++ data/data-structure.js
@@ -1,9 +1,18 @@
-//
+//Basic Data Structures: Copy an Array with the Spread Operator
+// While slice() allows us to be selective about what elements of an array to copy, among several other useful tasks, ES6's new spread operator allows us to easily copy all of an array's elements, in order, with a simple and highly readable syntax. The spread syntax simply looks like this: ...
+
+// In practice, we can use the spread operator to copy an array like so:
+
+// let thisArray = [true, true, undefined, false, null];
+// let thatArray = [...thisArray];
+// thatArray equals [true, true, undefined, false, null]
+// thisArray remains unchanged, and is identical to thatArray
+
 function copyMachine(arr, num) {
   let newArr = [];
   while (num >= 1) {
     // change code below this line
-
+    newArr = [...arr]; //it is not correct. we need the push() array method to copy the items from one array to another. 
     // change code above this line
     num--;
   }
@@ -18,7 +27,7 @@ console.log(copyMachine([true, false, true], 2));
       let newArr = [];
       while (num >= 1) {
         // change code below this line
-        newArr.push([...arr]);
+        newArr.push([...arr]); 
         // change code above this line
         num--;
       }
@@ -26,4 +35,4 @@ console.log(copyMachine([true, false, true], 2));
     }
 
     // change code here to test different cases:
-    console.log(copyMachine([true, false, true], 2)); //return 
+    console.log(copyMachine([true, false, true], 2)); //return true,false,true,true,false,true
